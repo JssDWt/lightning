ed2b0cf2312b5dbd581bc725fd56fd02d6b0c0c5
Christian Decker <decker.christian@gmail.com>
2024-08-12 17:00:56 +0200

    plugin: Add tracing support for `send_outreq`
    The `send_outreq` function is a good place to suspend and resume
    traces, since these are usually the places where we hand off control
    back to the `io_loop`. This assumes that we do not continue doing
    heavy liftin after we have queued an `outreq` call, but that is most
    likely the case anyway. This frees us from having to track suspensions
    whenever we call the RPC from a plugin.

--- plugins/libplugin-pay.c
+++ plugins/libplugin-pay.c
@@ -2516,7 +2516,6 @@ local_channel_hints_listpeerchannels(struct command *cmd, const char *buffer,
 				     const jsmntok_t *toks, struct payment *p)
 {
 	struct listpeers_channel **chans;
-	trace_span_resume(p);
 	chans = json_to_listpeers_channels(tmpctx, buffer, toks);
 
 	for (size_t i = 0; i < tal_count(chans); i++) {
@@ -2583,7 +2582,6 @@ static void local_channel_hints_cb(void *d UNUSED, struct payment *p)
 		return payment_continue(p);
 
 	trace_span_start("local_channel_hints_cb", p);
-	trace_span_suspend(p);
 	req = jsonrpc_request_start(p->plugin, NULL, "listpeerchannels",
 				    local_channel_hints_listpeerchannels,
 				    local_channel_hints_listpeerchannels, p);
@@ -3437,7 +3435,6 @@ static struct command_result *direct_pay_listpeerchannels(struct command *cmd,
 							  const jsmntok_t *toks,
 							  struct payment *p)
 {
-	trace_span_resume(p);
 	struct listpeers_channel **channels = json_to_listpeers_channels(tmpctx, buffer, toks);
 	struct direct_pay_data *d = payment_mod_directpay_get_data(p);
 
@@ -3489,7 +3486,6 @@ static void direct_pay_cb(struct direct_pay_data *d, struct payment *p)
 
 
 	trace_span_start("direct_pay_cb", p);
-	trace_span_suspend(p);
 	req = jsonrpc_request_start(p->plugin, NULL, "listpeerchannels",
 				    direct_pay_listpeerchannels,
 				    direct_pay_listpeerchannels,
